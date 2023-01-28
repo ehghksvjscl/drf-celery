e1090c19fdf96e3756dfa14fe3174325e42bc240
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-28 21:46:04 +0900

    fix: manage file env 기능 추가

--- manage.py
+++ manage.py
@@ -2,7 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import dotenv
+import platform
 
 def main():
     """Run administrative tasks."""
@@ -18,5 +19,8 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if platform.system() != "Linux":
+        dotenv.read_dotenv()
+
     main()
